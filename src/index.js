ecce3c8a44823079d8ada14a3dddf7411c607463
Esa Salminen <esa_salminen@hotmail.com>
2021-10-21 12:51:50 +0300

    6.2 tehty, 0,5h

--- src/index.js
+++ src/index.js
@@ -7,16 +7,31 @@ const store = createStore(reducer)
 
 const App = () => {
 
+  const good = store.getState().good
+  const ok = store.getState().ok
+  const bad = store.getState().bad
+  const all = good + ok + bad
+  const avg = all === 0 ? 0 : (good + (bad * -1)) / all
+  const positive = all === 0 ? 0 : good / all * 100
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={()=>store.dispatch({type: 'GOOD'})}>good</button> 
-      <button onClick={()=>store.dispatch({type: 'OK'})}>neutral</button> 
-      <button onClick={()=>store.dispatch({type: 'BAD'})}>bad</button>
-      <button onClick={()=>store.dispatch({type: 'ZERO'})}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>neutral</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button>
+      <h1>Statistics</h1>
+      {all === 0 ? <div>no feedback given</div> :
+        <>
+          <div>good {good}</div>
+          <div>neutral {ok}</div>
+          <div>bad {bad}</div>
+          <div>all {all}</div>
+          <div>avg {avg}</div>
+          <div>positive {positive} %</div>
+        </>
+      }
     </div>
   )
 }
